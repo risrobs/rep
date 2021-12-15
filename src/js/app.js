73aeb4713beea8b007a0d9debd8653ff48fa21be
risrobs <robertivanov22@abv.bg>
2021-12-15 14:16:34 +0200

    vf

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const product = document.querySelector(".product")
+  const price = document.querySelector(".price")
+  console.log(price.textContent)
 
+  product.setAttribute("data-price", price.textContent)
 });
